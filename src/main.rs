3b54ecf5c48cc0c2859cbbe3b9ac0e3f62f12028
m1911star <lhlxtl@gmail.com>
2022-05-24 23:22:43 +0800

    add transform animation example

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+mod animation_transform;
 mod area_light;
 mod button;
-mod practice;
 mod cubes;
+mod cursor;
 mod mesh;
 mod pbr;
+mod practice;
 mod shadow;
 mod wire;
-mod cursor;
 
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::pbr::wireframe::WireframePlugin;
+use bevy::render::render_resource::WgpuFeatures;
+use bevy::render::settings::WgpuSettings;
 use bevy::{prelude::*, utils::HashSet};
 use rand::{prelude::SliceRandom, Rng};
 use std::{
@@ -16,9 +20,6 @@ use std::{
     io::{self, BufRead, BufReader},
     process::Stdio,
 };
-use bevy::pbr::wireframe::WireframePlugin;
-use bevy::render::render_resource::WgpuFeatures;
-use bevy::render::settings::WgpuSettings;
 
 fn main() {
     // practice::run();
@@ -29,19 +30,20 @@ fn main() {
     // shadow::run();
     // mesh::run();
     // cubes::run();
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::WHITE))
-        .add_startup_system(cubes::set_up)
-        .add_startup_system(area_light::setup)
-        .add_startup_system(mesh::setup)
-        .add_startup_system(pbr::setup)
-        .insert_resource(Msaa { samples: 4 })
-        .insert_resource(WgpuSettings {
-            features: WgpuFeatures::POLYGON_MODE_LINE,
-            ..Default::default()
-        })
-        .add_plugin(WireframePlugin)
-        .add_startup_system(wire::setup)
-        .run();
+    animation_transform::run();
+    // App::new()
+    //     .add_plugins(DefaultPlugins)
+    //     .insert_resource(ClearColor(Color::WHITE))
+    //     .add_startup_system(cubes::set_up)
+    //     .add_startup_system(area_light::setup)
+    //     .add_startup_system(mesh::setup)
+    //     .add_startup_system(pbr::setup)
+    //     .insert_resource(Msaa { samples: 4 })
+    //     .insert_resource(WgpuSettings {
+    //         features: WgpuFeatures::POLYGON_MODE_LINE,
+    //         ..Default::default()
+    //     })
+    //     .add_plugin(WireframePlugin)
+    //     .add_startup_system(wire::setup)
+    //     .run();
 }
